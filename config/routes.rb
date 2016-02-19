d492e5311c03b829fd346355af816769923f8d45
nwichert <nick.wichert@icloud.com>
2016-02-18 18:13:21 -0600

    added welcome#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   devise_for :users
   resources :kanban_boards do
     resources :tasks do
@@ -11,10 +13,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'kanban_boards#index'
+  # root 'kanban_boards#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'kanban_boards' => 'kanban_boards#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
